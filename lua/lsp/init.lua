f8ceb81285e1ec7b1d28476d25770acc29eeec7b
Duckonaut <duckonaut@gmail.com>
2022-05-13 17:35:08 +0200

    Fix long standing LSP issue, add haskell formatter

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -3,31 +3,3 @@ if not status then return end
 
 require('lsp.lsp-installer')
 require('lsp.handlers').setup()
-
-local lsp_installer = require("nvim-lsp-installer")
-
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-        settings = {
-            ["rust-analyzer"] = {
-                checkOnSave = {
-                    command = "clippy",
-                }
-            }
-        }
-    }
-
-    if server.name == "rust_analyzer" then
-        -- Initialize the LSP via rust-tools instead
-        require("rust-tools").setup {
-            -- The "server" property provided in rust-tools setup function are the
-            -- settings rust-tools will provide to lspconfig during init.
-            -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
-            -- with the user's own settings (opts).
-            server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
-        }
-        server:attach_buffers()
-    else
-        server:setup(opts)
-    end
-end)
--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -31,6 +31,23 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", clangd_opts, opts)
 	 end
 
+     if server.name == "rust_analyzer" then
+         local rust_opts = require("lsp.settings.rust-analyzer")
+         require("rust-tools").setup {
+            -- The "server" property provided in rust-tools setup function are the
+            -- settings rust-tools will provide to lspconfig during init.
+            -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+            -- with the user's own settings (opts).
+            server = vim.tbl_deep_extend("force", server:get_default_options(), rust_opts),
+        }
+        server:attach_buffers()
+    end
+
+    if server.name == "hls" then
+        local haskell_opts = require("lsp.settings.hls")
+        opts = vim.tbl_deep_extend("force", haskell_opts, opts)
+    end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
