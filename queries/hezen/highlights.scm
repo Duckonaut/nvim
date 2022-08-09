feeb75bc22173c3468c47730aaedaddacf67d1a1
Duckonaut <duckonaut@gmail.com>
2022-08-09 22:32:17 +0200

    Hezen highlights

--- queries/hezen/highlights.scm
+++ queries/hezen/highlights.scm
@@ -9,6 +9,7 @@
     "break"
     "continue"
     "fn"
+    "class"
 ] @keyword
 
 [
@@ -34,6 +35,12 @@
 (function name: (identifier) @function)
 (function params: (parameters (identifier) @variable.parameter))
 
+(classDecl className: (identifier) @type)
+(classDecl classSuper: (identifier) @type)
+(classDecl classMethods: (function name: (identifier) @function.method))
+
+
 (string) @string
 (number) @number
 (builtin) @constant.builtin
+(super) @constant.builtin
