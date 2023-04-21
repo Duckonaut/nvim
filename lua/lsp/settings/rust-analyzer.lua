af03bac54bc6c859d6ca6cad2a5b69606e3de41f
Duckonaut <hasohasacz@gmail.com>
2023-04-21 12:43:10 +0200

    Rust analyzer patch un-break on windows

--- lua/lsp/settings/rust-analyzer.lua
+++ lua/lsp/settings/rust-analyzer.lua
@@ -1,4 +1,12 @@
+local mason_ra_path = "rust-analyzer"
+
+local status, mason_reg = pcall(require, "mason-registry")
+if status then
+    mason_ra_path = mason_reg.get_package("rust-analyzer"):get_install_path() .. "/rust-analyzer"
+end
+
 return {
+    cmd = { mason_ra_path },
     settings = {
         ["rust-analyzer"] = {
             checkOnSave = {
